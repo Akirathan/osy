4e5159d9c25569dc0985178df7ac17f1b64937eb
Pavel Marek <pavel.akira.marek@gmail.com>
2018-10-29 17:45:07 +0100

    Fix address conversion to frame index and vice versa

--- kernel/mm/my_falloc.c
+++ kernel/mm/my_falloc.c
@@ -1,13 +1,24 @@
 #include "my_falloc.h"
 #include <adt/bitmap.h>
 
+static bool mem_accessible(const uint32_t addr);
+static size_t scan_memory(void);
+static size_t count_bitmap_storage(const size_t frame_num);
+static uintptr_t frame_to_addr(const size_t frame_index);
+static size_t addr_to_frame(const uintptr_t addr);
+static bool is_addr_aligned(const uintptr_t addr);
+
 static bitmap_t bitmap;
-/* Address of memory (in KSEG1) from which the frames will be allocated. */
+/* Virtual address of memory (in KSEG1) from which the frames will be allocated. */
 static uintptr_t frames_begin_kseg = 0;
 /* Total number of usable frames. */
 static size_t usable_frames = 0;
 
 #define MAX_MEMORY_MB 512U
+
+#define KSEG1_BASE 0xA0000000
+#define VIRT_TO_PHYS(addr)  (((uintptr_t) addr) - (uintptr_t) KSEG1_BASE)
+#define PHYS_TO_VIRT(addr)  (((uintptr_t) addr) + (uintptr_t) KSEG1_BASE)
 
 /** Tests whether given address is usable
  * 
@@ -70,9 +81,29 @@ static size_t count_bitmap_storage(const size_t frame_num)
  * 
  * @return Physical address of the frame.
  */
-static uint32_t frame_to_addr(const size_t frame_index)
+static inline uintptr_t frame_to_addr(const size_t frame_index)
 {
-    return (frame_index * FRAME_SIZE);
+    return VIRT_TO_PHYS(frames_begin_kseg + (frame_index * FRAME_SIZE));
+}
+
+/** Converts physical address to frame index.
+ * 
+ * @return Index of the frame corresponding to @p addr.
+ */
+static inline size_t addr_to_frame(const uintptr_t addr)
+{
+    assert(is_addr_aligned(addr));
+
+    /* Shifting by FRAME_WIDTH is the same as dividing by FRAME_SIZE */
+    size_t frame_index = (PHYS_TO_VIRT(addr) - frames_begin_kseg) >> FRAME_WIDTH;
+    return frame_index;
+}
+
+static inline bool is_addr_aligned(const uintptr_t addr)
+{
+    uintptr_t base_addr = PHYS_TO_VIRT(addr) - frames_begin_kseg;
+    /* Check whether last 12 bits are 0. */
+    return ((base_addr % FRAME_SIZE) == 0);
 }
 
 void my_frame_init(void)
@@ -93,12 +124,13 @@ void my_frame_init(void)
  */
 int my_frame_alloc(uintptr_t *phys, const size_t cnt, const vm_flags_t flags)
 {
-    if (flags & VF_VA_AUTO) {
-        if (cnt == 0) {
-            return ENOMEM;
-        }
+    if (cnt == 0) {
+        return ENOMEM;
+    }
 
+    if (flags & VF_VA_AUTO) {
         size_t index = 0;
+        /* TODO: do not begin from 0 */
         bool err = bitmap_allocate_range(&bitmap, cnt, 0, bitmap.elements, &index);
         if (err != false) {
             /* Allocation successfull. */
@@ -111,8 +143,35 @@ int my_frame_alloc(uintptr_t *phys, const size_t cnt, const vm_flags_t flags)
         }
     }
     else if (flags & VF_VA_USER) {
-        /* Check memory allignment */
+        if (!is_addr_aligned(*phys)) {
+            return EINVAL;
+        }
+
+        bool err = bitmap_check_range(&bitmap, addr_to_frame(*phys), cnt);
+        if (err == true) {
+            /* Given frame range is full. */
+            return ENOMEM;
+        }
+        else {
+            return EOK;
+        }
     }
 
     return EINVAL;
+}
+
+int my_frame_free(const uintptr_t phys, const size_t cnt)
+{
+    return 0;
+}
+
+void my_frame_test(void)
+{
+    /* Test conversion from frame index to address and vice versa. */
+    for (size_t i = 0; i < 10; i++) {
+        uintptr_t addr = frame_to_addr(i);
+        size_t frame_index = addr_to_frame(addr);
+        assert(frame_index == i);
+    }
+
 }
--- kernel/mm/my_falloc.h
+++ kernel/mm/my_falloc.h
@@ -5,5 +5,7 @@
 
 void my_frame_init(void);
 int my_frame_alloc(uintptr_t *phys, const size_t cnt, const vm_flags_t flags);
+int my_frame_free (const uintptr_t phys, const size_t cnt);
+void my_frame_test(void);
 
 #endif
