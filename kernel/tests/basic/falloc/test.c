269ec80017d5d04e09023c3274f7bab15efa2fc0
Pavel Marek <pavel.akira.marek@gmail.com>
2018-10-29 17:45:15 +0100

    Add simple falloc test

--- kernel/tests/basic/falloc/test.c
+++ kernel/tests/basic/falloc/test.c
@@ -2,19 +2,34 @@
 #include "../../include/defs.h"
 #include "mm/my_falloc.h"
 
-static void test_allocation(void)
+static void my_test_allocation(void)
 {
-    uintptr_t addr = 0;
-    int err = my_frame_alloc(&addr, 2, VF_VA_AUTO);
+    uintptr_t first_addr = 0;
+    uintptr_t sec_addr = 0;
+    int err = my_frame_alloc(&first_addr, 2, VF_VA_AUTO);
     assert (err == EOK);
-    err = my_frame_alloc(&addr, 2, VF_VA_AUTO);
+
+    err = my_frame_alloc(&sec_addr, 2, VF_VA_AUTO);
+    assert(err == EOK);
+    assert(first_addr != sec_addr);
+
+    err = my_frame_alloc(&first_addr, 2, VF_VA_USER);
+    assert(err == ENOMEM);
+
+    err = my_frame_alloc(&sec_addr, 2, VF_VA_USER);
+    assert(err == ENOMEM);
+
+    /* Points at frame that is above sec_addr. */
+    uintptr_t big_addr = sec_addr + (2 * FRAME_SIZE);
+    err = my_frame_alloc(&big_addr, 2, VF_VA_USER);
     assert(err == EOK);
 }
 
 void test_run(void)
 {
     my_frame_init();
-    test_allocation();
+    my_frame_test();
+    my_test_allocation();
 
     puts("Falloc test passed\n");
 }
