5998216997b91a193bb3a0500b7e0971767e2795
Pavel Marek <pavel.akira.marek@gmail.com>
2018-10-29 18:07:28 +0100

    Add some bitmap tests

--- kernel/tests/basic/bitmap/test.c
+++ kernel/tests/basic/bitmap/test.c
@@ -16,14 +16,17 @@ static void test_range_succ(void)
     bitmap_init(&bitmap, 4, storage);
 
     size_t index = 0;
-    int err = bitmap_allocate_range(&bitmap, 2, 0, bitmap.elements, &index);
-    assert(err == 1);
+    bool err = bitmap_allocate_range(&bitmap, 2, 0, bitmap.elements, &index);
+    assert(err == true);
     assert(index == 0);
 
     err = bitmap_allocate_range(&bitmap, 2, 0, bitmap.elements, &index);
-    assert(err == 1);
+    assert(err == true);
     assert(index == 2);
-    (void) err;
+
+    /* Bitmap should look like this: [1,1,1,1] */
+    err = bitmap_check_range(&bitmap, 0, 4);
+    assert(err == true);
 }
 
 static void test_range_fail(void)
@@ -68,11 +71,40 @@ static void test_set_get(void)
     assert(bitmap_get(&bitmap, 4) == 1);
 }
 
+static void test_check_range(void)
+{
+    bitmap_t bitmap;
+    uint8_t storage[10];
+    clear(storage, 10);
+
+    bitmap_init(&bitmap, 5, storage);
+    bitmap_set_range(&bitmap, 0, 2);
+
+    /* This range is set */
+    int err = bitmap_check_range(&bitmap, 0, 2);
+    assert(err == true);
+
+    /* This range is empty */
+    err = bitmap_check_range(&bitmap, 2, 2);
+    assert(err == false);
+
+    bitmap_set(&bitmap, 2, 0);
+    bitmap_set_range(&bitmap, 3, 2);
+
+    /* Bitmap now looks: [1,1,0,1,1] */
+    err = bitmap_check_range(&bitmap, 0, 5);
+    assert(err == false);
+
+    err = bitmap_check_range(&bitmap, 1, 3);
+    assert(err == false);
+}
+
 void test_run(void)
 {
     test_range_succ();
     test_range_fail();
     test_set_get();
     test_storage_move();
+    test_check_range();
     puts("Bitmap test passed.\n");
 }
